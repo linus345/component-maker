606b8f18f555fb71b7922bb495af0fce29a7eff8
linus345 <linusekman01@gmail.com>
2020-04-06 18:50:50 +0200

    changed grid layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 const Layout = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 2fr 3fr;
   grid-template-rows: 1fr 1fr;
   grid-template-areas:
     "options view"
     "options code";
+  grid-gap: 1px;
   min-height: 100vh;
+  background-color: #222;
 `;
 
 export default Layout;
