d9e68d9bc9d7baa514b6223df6d176fe00cf23b2
linus345 <linusekman01@gmail.com>
2020-04-06 19:29:33 +0200

    created a box to show the current css styling with syntax highlighting

--- src/components/CodeSection.js
+++ src/components/CodeSection.js
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import hljs from 'highlight.js/lib/highlight';
+import 'highlight.js/styles/rainbow.css';
+import css from 'highlight.js/lib/languages/css';
 
-function CodeSection() {
+function stylesToCSS(styles) {
+  const copy = Object.assign({}, styles);
+  // format border property
+  const border = `${copy["border-width"]} solid ${copy["border-color"]}`;
+  copy["border"] = border;
+  delete copy["border-width"];
+  delete copy["border-color"];
+  return JSON.stringify(copy, null, 2)
+    .replace(/"/g, "")
+    .replace(/,/g, ";");
+}
+
+function CodeSection({ styles }) {
+  const codeRef = useRef(null);
+  const hlCode = stylesToCSS(styles);
+
+  useEffect(() => {
+    hljs.registerLanguage('css', css);
+  }, []);
+
+  useEffect(() => {
+    if(codeRef && codeRef.current) {
+      hljs.highlightBlock(codeRef.current);
+    }
+  }, [codeRef, styles]);
+
   return (
     <StyledCodeSection>
-      <h1>code</h1>
+      <div className="code-box">
+        <pre>
+          <code
+            ref={codeRef}
+            className="css"
+          >
+            {`button ${hlCode}`}
+          </code>
+        </pre>
+      </div>
     </StyledCodeSection>
   );
 }
 
 const StyledCodeSection = styled.div`
   grid-area: code;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+
+  .code-box {
+    width: 80%;
+    background-color: #17181b;
+    border-radius: 1px;
+    box-shadow:
+      0 0 0 3px #242425,
+      0 0 0 6px #131314,
+      0 0 0 9px #035cb9;
+    padding: 20px 25px;
+
+    pre {
+      code {
+        background-color: #17181b;
+      }
+    }
+  }
 `;
 
 export default CodeSection;
